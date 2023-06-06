ed7b1e4acbe4d53d8b87da5aba7e922886129f58
Nick Sheward <nickshwrd@gmail.com>
2023-06-06 10:18:29 +0100

    Created basic Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div>
+      <div>Don't Forget The Fish!</div>
+      <div>Score: </div>
+      <div>Best score: </div>
+    </div>
+  );
+};
+
+export default Header;
