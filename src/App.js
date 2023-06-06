82b7b11051be48080eb69f350aaa01eca2573b17
Nick Sheward <nickshwrd@gmail.com>
2023-06-06 10:40:36 +0100

    Created basic Cards component with click function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Cards from './components/Cards/Cards';
 
 const App = () => {
   return (
     <div>
-      <Header />
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Cards />
+      </div>
     </div>
   );
 };
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,73 @@
+import './Cards.css';
+import React, { useState } from 'react';
+
+const Cards = () => {
+  const [fish, setFishState] = useState([
+    {
+      order: 1,
+      fishType: 'Shark',
+    },
+    {
+      order: 2,
+      fishType: 'Squid',
+    },
+    {
+      order: 3,
+      fishType: 'Octopus',
+    },
+    {
+      order: 4,
+      fishType: 'Cod',
+    },
+    {
+      order: 5,
+      fishType: 'Salmon',
+    },
+    {
+      order: 6,
+      fishType: 'Eel',
+    },
+    {
+      order: 7,
+      fishType: 'Anchovy',
+    },
+    {
+      order: 8,
+      fishType: 'Crab',
+    },
+    {
+      order: 9,
+      fishType: 'Lobster',
+    },
+    {
+      order: 10,
+      fishType: 'Prawn',
+    },
+    {
+      order: 11,
+      fishType: 'Sword Fish',
+    },
+    {
+      order: 12,
+      fishType: 'Tuna',
+    },
+  ]);
+
+  const logFish = (fishType) => {
+    console.log(fishType);
+  };
+
+  return (
+    <div>
+      {fish.map((item) => {
+        return (
+          <div key={item.fishType} onClick={() => logFish(item.fishType)}>
+            {item.fishType}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Cards;
